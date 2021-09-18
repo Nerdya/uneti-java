acd07a65edde2c9653ed5355fb82b60d9d2de5ab
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2021-09-18 17:57:01 +0700

    Update day3

--- src/day3/HinhChuNhat.java
+++ src/day3/HinhChuNhat.java
@@ -6,7 +6,7 @@ public class HinhChuNhat {
 
 	public HinhChuNhat() {
 	}
-	
+
 	public HinhChuNhat(double a, double b) {
 		if (a >= b) {
 			this.dai = a;
@@ -16,28 +16,28 @@ public class HinhChuNhat {
 			this.rong = a;
 		}
 	}
-	
+
 	public double getDai() {
-        return dai;
-    }
+		return dai;
+	}
 
-    public void setDai(double dai) {
-        this.dai = dai;
-    }
+	public void setDai(double dai) {
+    	this.dai = dai;
+	}
 
-    public double getRong() {
-        return rong;
-    }
+	public double getRong() {
+		return rong;
+	}
 
-    public void setRong(double rong) {
-        this.rong = rong;
-    }
-    
-    public double ChuVi() {
-        return 2 * (this.dai + this.rong);
-    }
-    
-    public double DienTich() {
-    	return this.dai * this.rong;
-    }
+	public void setRong(double rong) {
+    	this.rong = rong;
+	}
+
+	public double ChuVi() {
+    	return 2 * (this.dai + this.rong);
+	}
+
+	public double DienTich() {
+		return this.dai * this.rong;
+	}
 }
