e0fa62b890e3d6897542a0c6ff4a39957d6e9494
Nguyen Kim Cuong <cuongnk@thinhtientech.com>
2021-09-18 17:52:41 +0700

    Update day3

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Main extends HinhChuNhat {
 	public static void main(String[] args) {
-//		HinhChuNhat HCN = new HinhChuNhat();
-//		Scanner scanner = new Scanner(System.in);
-//		
+		Scanner scanner = new Scanner(System.in);
+
 //		System.out.println("Nhap chieu dai: ");
 //		double dai = scanner.nextDouble();
-//		HCN.setDai(dai);
 //		System.out.println("Nhap chieu rong: ");
 //		double rong = scanner.nextDouble();
+
+//		HinhChuNhat HCN = new HinhChuNhat();
+//		HCN.setDai(dai);
 //		HCN.setRong(rong);
-//		
 //		System.out.println("Chu vi = " + HCN.ChuVi());
 //		System.out.println("Dien tich = " + HCN.DienTich());
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhap canh a: ");
-		double a = scanner.nextDouble();
-		System.out.println("Nhap canh b: ");
-		double b = scanner.nextDouble();
-		
+
+		double a;
+		do {
+			System.out.println("Canh a: ");
+			a = scanner.nextDouble();
+		} while (a <= 0);
+		double b;
+		do {
+			System.out.println("Canh b: ");
+			b = scanner.nextDouble();
+		} while (b <= 0);
+
 		HinhChuNhat hcn = new HinhChuNhat(a, b);
 		System.out.println("Chu vi = " + hcn.ChuVi());
 		System.out.println("Dien tich = " + hcn.DienTich());
